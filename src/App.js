18f787c29a3a1e19fda709c61d904bad646dbf20
rahul-bfc <rahul@bfcdata.com>
2019-11-06 11:21:43 +0530

    fix grid missing in lower resolutions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
   return (
     <div className="App">
       <div class="flex flex-wrap mb-4">
-        <div className="lg:w-1/2 md:w-full red h-12"></div>
-        <div className="lg:w-1/2 md:w-full black h-12"></div>
-        <div className="lg:w-1/3 sm:w-full md:w-1/2 grey h-12"></div>
-        <div className="lg:w-1/3 sm:w-full md:w-1/2 purple h-12"></div>
-        <div className="lg:w-1/3 sm:w-full md:w-1/2 pink h-12"></div>
+        <div className="w-full lg:w-1/2 md:w-full red h-12"></div>
+        <div className="w-full lg:w-1/2 md:w-full black h-12"></div>
+        <div className="w-full lg:w-1/3 sm:w-full md:w-1/2 grey h-12"></div>
+        <div className="w-full lg:w-1/3 sm:w-full md:w-1/2 purple h-12"></div>
+        <div className="w-full lg:w-1/3 sm:w-full md:w-1/2 pink h-12"></div>
       </div>
     </div>
   );
